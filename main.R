2631fba30a4890c57fc60c116d9f810ddcab1e90
JulienCRB <128613221+JulienCRB@users.noreply.github.com>
2023-06-01 11:27:00 +0200

    Update main.R

--- main.R
+++ main.R
@@ -14,6 +14,14 @@ ggplot(data, aes(x = age)) +
 
 #distribution des ethnies sur le jeu de données
 
+data$race <- gsub("Asian or Pacific Islander", "Asian/Pacific", data$race)
+data$race <- gsub("Amer Indian Aleut or Eskimo", "Natives", data$race)
+
+
+ggplot(data, aes(x = race)) +
+  geom_bar() +
+  labs(title = "Distribution des ethnies", x = "Ethnies", y = "Distribution")
+
 
 
 # income level by race (not relevant with 50000+, too crowded)
